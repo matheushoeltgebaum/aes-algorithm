0e61bbff28e11682bb70ee27f01ad22e2bd38882
matheushoeltgebaum <pereiramateduardo@gmail.com>
2019-06-03 18:55:01 -0300

    Initial commit

---
+++ AES_Algorithm/AES_Algorithm/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿using System.Windows.Forms;
+
+namespace AES_Algorithm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbArquivo = new System.Windows.Forms.Label();
+            this.tbFile = new System.Windows.Forms.TextBox();
+            this.btnEncrypt = new System.Windows.Forms.Button();
+            this.tbDirectory = new System.Windows.Forms.TextBox();
+            this.lbDiretorioExtracao = new System.Windows.Forms.Label();
+            this.rtbLog = new System.Windows.Forms.RichTextBox();
+            this.lbCipherKey = new System.Windows.Forms.Label();
+            this.tbCipherKey = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lbArquivo
+            // 
+            this.lbArquivo.AutoSize = true;
+            this.lbArquivo.Location = new System.Drawing.Point(12, 9);
+            this.lbArquivo.Name = "lbArquivo";
+            this.lbArquivo.Size = new System.Drawing.Size(46, 13);
+            this.lbArquivo.TabIndex = 0;
+            this.lbArquivo.Text = "Arquivo:";
+            // 
+            // tbFile
+            // 
+            this.tbFile.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbFile.Location = new System.Drawing.Point(129, 6);
+            this.tbFile.Name = "tbFile";
+            this.tbFile.Size = new System.Drawing.Size(520, 20);
+            this.tbFile.TabIndex = 1;
+            this.tbFile.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tbArquivo_KeyDown);
+            // 
+            // btnEncrypt
+            // 
+            this.btnEncrypt.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnEncrypt.Location = new System.Drawing.Point(574, 84);
+            this.btnEncrypt.Name = "btnEncrypt";
+            this.btnEncrypt.Size = new System.Drawing.Size(75, 23);
+            this.btnEncrypt.TabIndex = 2;
+            this.btnEncrypt.Text = "Criptografar";
+            this.btnEncrypt.UseVisualStyleBackColor = true;
+            this.btnEncrypt.Click += new System.EventHandler(this.btnEncrypt_Click);
+            // 
+            // tbDirectory
+            // 
+            this.tbDirectory.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbDirectory.Location = new System.Drawing.Point(129, 32);
+            this.tbDirectory.Name = "tbDirectory";
+            this.tbDirectory.Size = new System.Drawing.Size(520, 20);
+            this.tbDirectory.TabIndex = 4;
+            this.tbDirectory.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tbDiretorio_KeyDown);
+            // 
+            // lbDiretorioExtracao
+            // 
+            this.lbDiretorioExtracao.AutoSize = true;
+            this.lbDiretorioExtracao.Location = new System.Drawing.Point(12, 35);
+            this.lbDiretorioExtracao.Name = "lbDiretorioExtracao";
+            this.lbDiretorioExtracao.Size = new System.Drawing.Size(107, 13);
+            this.lbDiretorioExtracao.TabIndex = 3;
+            this.lbDiretorioExtracao.Text = "Local de exportação:";
+            // 
+            // rtbLog
+            // 
+            this.rtbLog.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.rtbLog.Location = new System.Drawing.Point(12, 113);
+            this.rtbLog.Name = "rtbLog";
+            this.rtbLog.ReadOnly = true;
+            this.rtbLog.Size = new System.Drawing.Size(637, 314);
+            this.rtbLog.TabIndex = 5;
+            this.rtbLog.Text = "";
+            // 
+            // lbCipherKey
+            // 
+            this.lbCipherKey.AutoSize = true;
+            this.lbCipherKey.Location = new System.Drawing.Point(12, 61);
+            this.lbCipherKey.Name = "lbCipherKey";
+            this.lbCipherKey.Size = new System.Drawing.Size(111, 13);
+            this.lbCipherKey.TabIndex = 6;
+            this.lbCipherKey.Text = "Chave de criptografia:";
+            // 
+            // tbCipherKey
+            // 
+            this.tbCipherKey.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbCipherKey.Location = new System.Drawing.Point(129, 58);
+            this.tbCipherKey.Name = "tbCipherKey";
+            this.tbCipherKey.Size = new System.Drawing.Size(520, 20);
+            this.tbCipherKey.TabIndex = 7;
+            this.tbCipherKey.Text = "65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(661, 439);
+            this.Controls.Add(this.tbCipherKey);
+            this.Controls.Add(this.lbCipherKey);
+            this.Controls.Add(this.rtbLog);
+            this.Controls.Add(this.tbDirectory);
+            this.Controls.Add(this.lbDiretorioExtracao);
+            this.Controls.Add(this.btnEncrypt);
+            this.Controls.Add(this.tbFile);
+            this.Controls.Add(this.lbArquivo);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "BJM_AES - Criptografia AES";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label lbArquivo;
+        private TextBox tbFile;
+        private Button btnEncrypt;
+        private TextBox tbDirectory;
+        private Label lbDiretorioExtracao;
+        private RichTextBox rtbLog;
+        private Label lbCipherKey;
+        private TextBox tbCipherKey;
+    }
+}
+
---
+++ AES_Algorithm/AES_Algorithm/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AES_Algorithm
+{
+    public partial class Form1 : Form
+    {
+        private string PlainFile { get { return tbFile.Text; } set { tbFile.Text = value; } }
+        private string PathExport { get { return tbDirectory.Text; } set { tbDirectory.Text = value; } }
+
+        private string CipherKey { get { return tbCipherKey.Text; } }
+
+        private byte[,] Key { get; set; } = new byte[4, 4];
+
+        private List<byte[,]> RoundKeys = new List<byte[,]>(11);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void tbArquivo_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.F4)
+                SelectFile();
+        }
+
+        private void tbDiretorio_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.F4)
+                SelectPath();
+        }
+
+        private void SelectFile()
+        {
+            using (var dialog = new OpenFileDialog())
+            {
+                dialog.CheckFileExists = true;
+                dialog.CheckPathExists = true;
+                dialog.Filter = "All files (*.*)|*.*";
+
+                if (dialog.ShowDialog() == DialogResult.OK)
+                    PlainFile = dialog.FileName;
+            }
+        }
+
+        private void SelectPath()
+        {
+            using (var dialog = new FolderBrowserDialog())
+            {
+                if (dialog.ShowDialog() == DialogResult.OK)
+                    PathExport = dialog.SelectedPath;
+            }
+        }
+
+        private void btnEncrypt_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //if (!string.IsNullOrEmpty(PlainFile) && !string.IsNullOrEmpty(PathExport))
+                //{
+                GenerateMatrixKey();
+                PrintKey();
+                GenerateRoundKeys();
+                //}
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show($"Ocorreu um erro no programa. Mensagem: {exc.Message}");
+            }
+        }
+
+        private void GenerateMatrixKey()
+        {
+            if (!string.IsNullOrEmpty(CipherKey))
+            {
+                string[] words = CipherKey.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+                byte[] bytes = new byte[16];
+
+                for (var i = 0; i < words.Length; i++)
+                    bytes[i] = byte.Parse(words[i]);
+
+                for (int i = 0; i < 4; i++)
+                {
+                    for (int j = 0; j < 4; j++)
+                    {
+                        int currentIndex = (i * 4) + j;
+                        var a = words[currentIndex];
+                        Key[j, i] = bytes[(i * 4) + j];
+                    }
+                }
+            }
+            else
+            {
+                throw new Exception("Não foi informada uma chave de criptografia!");
+            }
+        }
+
+        private void PrintKey()
+        {
+            rtbLog.AppendText("****Chave****\n");
+            for (var i = 0; i < 4; i++)
+                rtbLog.AppendText($"0x{Key[i, 0].ToString("X2")} 0x{Key[i, 1].ToString("X2")} 0x{Key[i, 2].ToString("X2")} 0x{Key[i, 3].ToString("X2")}\n");
+        }
+
+        private void GenerateRoundKeys()
+        {
+            RoundKeys.Add(Key);
+
+            for (var i = 1; i < 11; i++)
+            {
+                var lastWord = RoundKeys[i - 1].GetColumn(3);
+                var firstWord = GenerateFirstWord(lastWord);
+            }
+        }
+
+        private byte[] GenerateFirstWord(byte[] lastWord)
+        {
+            return new byte[10];
+        }
+    }
+}
